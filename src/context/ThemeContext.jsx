a81186dd82342be144c3f703e4b2b97acff2a279
Prabhat Pathak <klaus007p@gmail.com>
2026-08-14 19:39:53 +0530

    Added dark and light theme to local storage and if users had saved theme then it will apply also added keyboard shortcut to toogle

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -11,4 +11,37 @@ const getInitialTheme = () => {
 
     const modeDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     return modeDark ? 'dark' : 'light';
-}
+};
+
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState(getInitialTheme);
+
+    const toggleTheme = () => {
+        setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
+    };
+
+    // By applying theme to body and saving in localstorage
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    // ADDITIONAL :- Keyboard shortcut to toggle := press " T "
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if(e.key.toLowerCase() === 't') toggleTheme();
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    },[]);
+
+    return(
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
